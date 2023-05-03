b574ae8f881004d8e30cc28a781db8779244883f
Sachith Gunasekara <sachith@surge.global>
2023-05-04 00:35:29 +0530

    Refactor MLP class to use Equinox

--- model.py
+++ model.py
@@ -11,6 +11,9 @@ import math
 import inspect
 from dataclasses import dataclass
 
+import jax
+import jax.numpy as jnp
+import equinox as eqx
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -83,13 +86,16 @@ class CausalSelfAttention(nn.Module):
         y = self.resid_dropout(self.c_proj(y))
         return y
 
-class MLP(nn.Module):
+class MLP(eqx.Module):
+    c_fc    : eqx.nn.Linear
+    c_proj  : eqx.nn.Linear
+    dropout : eqx.nn.Dropout
 
     def __init__(self, config):
         super().__init__()
-        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
-        self.dropout = nn.Dropout(config.dropout)
+        self.c_fc    = eqx.nn.Linear(config.n_embd, 4 * config.n_embd, use_bias=config.bias)
+        self.c_proj  = eqx.nn.Linear(4 * config.n_embd, config.n_embd, use_bias=config.bias)
+        self.dropout = eqx.nn.Dropout(config.dropout)
 
     def forward(self, x):
         x = self.c_fc(x)
