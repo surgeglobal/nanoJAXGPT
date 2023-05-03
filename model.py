237c935ae68f2adf8e454eee9b862caa438b9e7a
Sachith Gunasekara <sachith@surge.global>
2023-05-04 00:44:56 +0530

    Remove custom LayerNorm in original source
    PyTorch does not support setting the bias to be optional for the LayerNorm module. The original source fixed this by writing the LayerNorm from scratch to allow bias to be optional. But since Equinox is able to incorporate this, it has been removed.
    
    Rolling back to default eqx.nn.LayerNorm.

--- model.py
+++ model.py
@@ -28,18 +28,6 @@ def new_gelu(x):
     return 0.5 * x * (1.0 + jnp.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * jnp.power(x, 3.0))))
 
 
-class LayerNorm(nn.Module):
-    """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
-
-    def __init__(self, ndim, bias):
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(ndim))
-        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
-
-    def forward(self, input):
-        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
-
-
 class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
