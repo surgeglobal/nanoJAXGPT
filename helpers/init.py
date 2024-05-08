68c719713136490214763ab8b8c04c000ea0b753
Chandeepa Dissanayake <chandeepa@surge.global>
2024-05-08 18:23:31 +0530

    Issue with not returning fixed.

--- helpers/init.py
+++ helpers/init.py
@@ -8,8 +8,10 @@ import jax
 
 
 def normal_(array: jax.Array, mean: float, std: float, key: jax.random.PRNGKey = jax.random.PRNGKey(0)) -> None:
-    array = jax.random.normal(key, array.shape) * std + mean
+    new_array = jax.random.normal(key, array.shape) * std + mean
+    return new_array
 
 
 def zeros_(array: jax.Array) -> None:
-    array = jax.numpy.zeros(array.shape)
+    new_array = jax.numpy.zeros(array.shape)
+    return new_array
